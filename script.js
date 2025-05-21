0c99492fb205d0af0bb8bacffc7de6aee769af52
Haydenbobo <boboxode@gmail.com>
2025-05-20 23:54:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact Form Submission
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for reaching out to BOBO ENGINEERING!");
+  this.reset();
+});
